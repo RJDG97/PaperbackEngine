db65d73e91da87187e95d4a82492915d635029df
LEE JUN PU <l.junpu@digipen.edu.sg>
2020-05-30 18:14:57 +0800

    Added sound, windows and input

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,54 @@
 #include "Source.h"
 
-int main()
-{
+#include "WindowsSystem.h"
+#include <memory>
+#include <iostream>
 
+int running = 1;
+FILE* file;
+void createdebugwindow();
+
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR cmdLine, INT cmdCount) {
+
+    UNREFERENCED_PARAMETER(previousInstance);
+    UNREFERENCED_PARAMETER(cmdLine);
+
+    // Checking for memory leaks
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    createdebugwindow();
+#endif
+
+    // Register the window class
+    WindowsSystem::Instance()->Init(currentInstance, "GAM200 MonkeyTypewriters", 800, 600);
+
+
+    // Display Window
+    ShowWindow(WindowsSystem::Instance()->getHandle(), cmdCount);
+    UpdateWindow(WindowsSystem::Instance()->getHandle());
+
+    while (running)
+    {
+        // Process windows messages
+        WindowsSystem::Instance()->ProcessMessage();
+
+    }
+
+    WindowsSystem::Instance()->UnloadInstance();
+    return 0;
 }
 
+void createdebugwindow() {
+    // Create Debug Window
+    if (AllocConsole())
+    {
+        LPCWSTR Name = L"Debug Window";
+        // Message stuff (lets you use print in command)
+        freopen_s(&file, "CONOUT$", "wt", stdout);
+        std::cout << "hi" << std::endl;
+        freopen_s(&file, "CONOUT$", "wt", stderr);
+        freopen_s(&file, "CONOUT$", "wt", stdin);
+
+        SetConsoleTitle(Name);
+    }
+}
---
+++ WindowsSystem.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef _WINDOWS_SYSTEM_H_
+#define _WINDOWS_SYSTEM_H_
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM param, LPARAM lparam);
+
+class WindowsSystem {
+	// Singleton declaration
+	static WindowsSystem* w_Instance;
+	// Windows class declaration
+	WNDCLASS wc;
+	// Message class declaration
+	MSG msg;
+	// Windows handle
+	HWND hwnd;
+
+
+
+public:
+	
+	// Constructor for WindowsSystem
+	WindowsSystem();
+	// Initializes the window, cursor, background
+	void Init(HINSTANCE _currentInstance, const char* _windowName, int _x, int _y,
+		LPCSTR _className = "MyWindowsSystem",
+		HCURSOR _cursor = LoadCursor(nullptr, IDC_ARROW),
+		HBRUSH _bgColor = (HBRUSH)COLOR_WINDOW);
+
+	// Processes messages
+	void ProcessMessage();
+
+	// Static function declaration for single WindowsSystem
+	static WindowsSystem* Instance();
+
+	// Unload WindowsSystem
+	void UnloadInstance();
+
+	HWND getHandle();
+};
+
+#endif
