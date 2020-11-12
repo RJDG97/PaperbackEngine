4e824632b5c3efa7e2335594e932c26b8f861880
Renzo Garcia <renzo_garcia@live.com>
2020-11-13 00:18:11 +0800

    Comments for my side :)

--- Include/Script/GeneralScripts.h
+++ Include/Script/GeneralScripts.h
@@ -11,18 +11,53 @@ namespace GeneralScripts
 	extern EntityID player_id;
 	extern Transform* player_rigidbody;
 	extern Transform* obj_rigidbody;
+	extern Status* player_status;
 	extern AMap* map_;
 	extern ForcesManager* forces_;
 
-	//AI* obj_{nullptr};
+	/******************************************************************************/
+	/*!
+	  \fn Chase(AIIt obj)
+
+	  \brief Object Chase player
+	*/
+	/******************************************************************************/
 	bool Chase(AIIt obj);
 
+	/******************************************************************************/
+	/*!
+	  \fn DetectPlayer(AIIt obj)
+
+	  \brief Check whether Object can detect player
+	*/
+	/******************************************************************************/
 	bool DetectPlayer(AIIt obj);
 	
+	/******************************************************************************/
+	/*!
+	  \fn AIHandler(AIIt obj)
+
+	  \brief Handle AI Logic
+	*/
+	/******************************************************************************/
 	void AIHandler(AIIt obj);
 
+	/******************************************************************************/
+	/*!
+	  \fn GetType(std::string type)
+
+	  \brief Return AI Types
+	*/
+	/******************************************************************************/
 	AI::AIType GetType(std::string type);
 
+	/******************************************************************************/
+	/*!
+	  \fn Patrol(AIIt obj)
+
+	  \brief Set Enemy to Patrol waypoints
+	*/
+	/******************************************************************************/
 	void Patrol(AIIt obj);
 }
 
