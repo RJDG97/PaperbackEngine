2c46ac2485bbbc0d710218dca58ed75e5521b2e0
Bryan Low <crazygamer1997@gmail.com>
2021-04-19 14:19:23 +0800

    Adjusted menu buttons Disabled debug logging

--- Source/Systems/Debug.cpp
+++ Source/Systems/Debug.cpp
@@ -19,47 +19,40 @@
 EngineDebug* EngineDebug::d_instance_ = 0;
 
 EngineDebug::EngineDebug() {
-	my_file_.open("Resources/Debug.txt", std::fstream::in | std::fstream::out | std::fstream::trunc);
+	//my_file_.open("Resources/Debug.txt", std::fstream::in | std::fstream::out | std::fstream::trunc);
 }
 
 EngineDebug::~EngineDebug() {
-	std::cout << "Debug dtor called" << std::endl;
-	if (my_file_.is_open()) {
+	//std::cout << "Debug dtor called" << std::endl;
+	/*if (my_file_.is_open()) {
 		my_file_.close();
-	}
+	}*/
 }
 
 void EngineDebug::WriteDebugMessage(const std::string& str) {
 
-	#if defined(DEBUG) | defined(_DEBUG)
+	/*#if defined(DEBUG) | defined(_DEBUG)
 	if (my_file_.is_open()) {
 		my_file_ << str;
 	}
 	#endif
-
+	*/
 	(void)str;
 }
 
 void EngineDebug::SaveDebug() {
 
-	my_file_.close();
+	/*my_file_.close();
 
-	my_file_.open("Resources/Debug.txt", std::ios_base::app);
+	my_file_.open("Resources/Debug.txt", std::ios_base::app);*/
 }
 
 bool EngineDebug::MyAssert(const char* expr_str, const char* file, size_t line, const char* return_message) {
-	//prepare string format to print into file
-	std::stringstream stream;
 
-	stream << "\nERROR HAS OCCURRED BEGIN LOG\nError Expression: " << expr_str 
-			<< "\nFile: " << file << "\nLine: " << line << "\nError Message: " << return_message
-			<< "\nERROR HAS OCCURRED END LOG\n";
-
-	std::cerr << stream.str();
-
-	WriteDebugMessage(stream.str());
-
-	SaveDebug();
+	(void)expr_str;
+	(void)file;
+	(void)line;
+	(void)return_message;
 
 	Message m{ MessageIDTypes::EXIT };
 	CORE->BroadcastMessage(&m);
